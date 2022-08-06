a0903c1dccd1e837fa31e6bff4fa4a3f984ead54
Rahul Kumar <86316159+rahul17081998@users.noreply.github.com>
2022-08-06 16:16:01 +0530

    Update Evaluate_Division.cpp

--- Graph/Evaluate_Division.cpp
+++ Graph/Evaluate_Division.cpp
@@ -8,7 +8,12 @@ where you must find the answer for Cj / Dj = ?.
 
 Return the answers to all queries. If a single answer cannot be determined, return -1.0.
 
-
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
 */
 
 
