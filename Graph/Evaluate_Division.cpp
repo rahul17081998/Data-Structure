5f9b2a5ec8bb80b9603493d42bfd5ba3e8645dfb
Rahul Kumar <86316159+rahul17081998@users.noreply.github.com>
2022-08-06 16:15:35 +0530

    Update Evaluate_Division.cpp

--- Graph/Evaluate_Division.cpp
+++ Graph/Evaluate_Division.cpp
@@ -1,3 +1,17 @@
+/*
+You are given an array of variable pairs equations and an array of real numbers values, 
+where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. 
+Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query 
+where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+
+*/
+
+
 class Solution {
     unordered_map<string, vector<pair<string, double>>> adjlist;
     unordered_map<string, bool> vis;
