396089a13400b639b4adec70d0b0150c4094942a
Rahul Kumar <86316159+rahul17081998@users.noreply.github.com>
2022-08-10 21:07:12 +0530

    Update Minimum_Height_Trees.cpp

--- Graph/Minimum_Height_Trees.cpp
+++ Graph/Minimum_Height_Trees.cpp
@@ -1,6 +1,9 @@
 /*
 The basic idea is "keep deleting leaves layer-by-layer, until reach the root."
-Specifically, first find all the leaves, then remove them. After removing, some nodes will become new leaves. So we can continue remove them. Eventually, there is only 1 or 2 nodes left. If there is only one node left, it is the root. If there are 2 nodes, either of them could be a possible root.
+Specifically, first find all the leaves, then remove them. After removing, some nodes will become new leaves. 
+So we can continue remove them. 
+Eventually, there is only 1 or 2 nodes left. If there is only one node left, it is the root. 
+If there are 2 nodes, either of them could be a possible root.
 Time Complexity: Since each node will be removed at most once, the complexity is O(n).
 */
 
