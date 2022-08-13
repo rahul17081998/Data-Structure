15faaf19dece716441180e966762dc9e27a74c9f
Rahul Kumar <86316159+rahul17081998@users.noreply.github.com>
2022-08-13 16:49:01 +0530

    Update Longest_Palindromic_Substring.cpp

--- Dynamic Programming/Longest_Palindromic_Substring.cpp
+++ Dynamic Programming/Longest_Palindromic_Substring.cpp
@@ -7,12 +7,14 @@ public:
         int dp[n][n];
         memset(dp, 0, sizeof(dp));
         // initialize with base case
+        // single letters are always palindromes
         for(int i=0; i<n; i++)
             dp[i][i]=1;
         
         int start=0;
         int end=1;
         
+        // check two consecutive letter are palindrome
         for(int i=0; i<n-1; i++){
             if(s[i]==s[i+1])
             {
@@ -23,13 +25,15 @@ public:
                 
         }
             
-        
+        // for more than 2 length string
         for(int j=2; j<n; j++)
         {
             for(int i=0; i<n-j; i++)
             {
                 int left=i; // starting point
                 int right=i+j; // endong point
+                // check if first and last position are same and inside 
+                // is palindrome --> means this string is also a palindrome
                 if(dp[left+1][right-1]==1 && s[left]==s[right]){
                     dp[left][right]=1;
                     start=i, end=j+1;
