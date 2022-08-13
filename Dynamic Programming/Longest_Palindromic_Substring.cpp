67db414f1379b288e62d6b8700e44b0489ed3fb8
Rahul Kumar <86316159+rahul17081998@users.noreply.github.com>
2022-08-13 16:45:49 +0530

    Create Longest_Palindromic_Substring.cpp

---
+++ Dynamic Programming/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        // Method 2; apply dynamic programming
+        int n=s.length();
+        int dp[n][n];
+        memset(dp, 0, sizeof(dp));
+        // initialize with base case
+        for(int i=0; i<n; i++)
+            dp[i][i]=1;
+        
+        int start=0;
+        int end=1;
+        
+        for(int i=0; i<n-1; i++){
+            if(s[i]==s[i+1])
+            {
+                dp[i][i+1]=1;
+                start=i;
+                end=2;
+            }
+                
+        }
+            
+        
+        for(int j=2; j<n; j++)
+        {
+            for(int i=0; i<n-j; i++)
+            {
+                int left=i; // starting point
+                int right=i+j; // endong point
+                if(dp[left+1][right-1]==1 && s[left]==s[right]){
+                    dp[left][right]=1;
+                    start=i, end=j+1;
+                }
+            }
+        }
+        return s.substr(start, end);
+        /*
+        // Method 1: time = O(N^2), space=O(1)
+        // iterative version
+        int n=s.length();
+        string ans="";
+        ans[0]=s[0];
+        
+        for(int i=0; i<n; i++){
+            string temp1="", temp2="";
+            for(int j=i; j<n; j++){
+                temp1 = temp1 + s[j];
+                temp2 = s[j]+temp2;
+                if(temp1==temp2 && temp1.length() > ans.length())
+                    ans = temp1;
+            }
+        }
+        return ans;
+        */
+    }
+};
